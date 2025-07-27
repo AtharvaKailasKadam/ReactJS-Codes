d228e6f762b70224a1f659725f7e88afebf5c5b2
Your Name <atharvakailaskadam1809@gmail.com>
2025-07-27 10:45:36 +0530

    Done with Dynamic Values

--- Lec_No_2-2ndReactProject/src/App.jsx
+++ Lec_No_2-2ndReactProject/src/App.jsx
@@ -1,25 +1,41 @@
+// Now Using the React Fragments.
+
 export let App = () => {
   return (
-    <div>
+    // Can also use the '<Fragment>  </Fragment>' as a Parent div.
+    <>
       <NetflixSeries />
       <NetflixSeries />
       <NetflixSeries />
       <NetflixSeries />
       <NetflixSeries />
       <NetflixSeries />
-    </div>
+    </>
   );
 };
 
+// We can use the Dynamic Values in this.
+// 1. Variable Values.
+// 2. Expressions.
+// 3. Functions.
+function GenreFun ()
+{
+  return "Action , Drama , Action , Adventure";
+}
+
 const NetflixSeries = () => {
+  const Name = "Game of Thrones";
+  const Seasons = 8;
+  const Ratings = 9.2;
   return (
     <div>
       <div>
         <img src = "GOT.jpg" alt = "GOT" width = "40%" height = "40%" />
       </div>
-      <h1> Name: Game of Thrones. </h1>
-      <h3> Seasons : 8 </h3>
-      <h3> Ratings : 9.2/10 </h3>
+      <h1> Name: {Name} </h1>
+      <h3> Seasons : {Seasons} </h3>
+      <h3> Ratings : {Ratings}/10 </h3>
+      <h3> Genre : {GenreFun()} </h3>
       <p> <b>Game of Thrones :</b> is an epic fantasy television series based on George R.R. Martin’s book series A Song of Ice and Fire. Set in the fictional continents of Westeros and Essos, the story follows the power struggles among noble families as they vie for control of the Iron Throne. Blending political intrigue, war, betrayal, and mythical elements like dragons and White Walkers, the series explores themes of loyalty, ambition, and survival. Known for its complex characters, unexpected plot twists, and morally grey choices, Game of Thrones captivated a global audience and became one of the most iconic TV shows of the 21st century.</p>
     </div>
   )

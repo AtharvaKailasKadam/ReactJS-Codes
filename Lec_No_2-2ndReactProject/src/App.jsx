6363e06964b22f40732c417563c98b62b3d6aeac
Your Name <atharvakailaskadam1809@gmail.com>
2025-07-27 10:16:47 +0530

    Created the Components

--- Lec_No_2-2ndReactProject/src/App.jsx
+++ Lec_No_2-2ndReactProject/src/App.jsx
@@ -1,6 +1,19 @@
 export let App = () => {
   return (
     <div>
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+    </div>
+  );
+};
+
+const NetflixSeries = () => {
+  return (
+    <div>
       <div>
         <img src = "GOT.jpg" alt = "GOT" width = "40%" height = "40%" />
       </div>
@@ -9,6 +22,7 @@ export let App = () => {
       <h3> Ratings : 9.2/10 </h3>
       <p> <b>Game of Thrones :</b> is an epic fantasy television series based on George R.R. Martin’s book series A Song of Ice and Fire. Set in the fictional continents of Westeros and Essos, the story follows the power struggles among noble families as they vie for control of the Iron Throne. Blending political intrigue, war, betrayal, and mythical elements like dragons and White Walkers, the series explores themes of loyalty, ambition, and survival. Known for its complex characters, unexpected plot twists, and morally grey choices, Game of Thrones captivated a global audience and became one of the most iconic TV shows of the 21st century.</p>
     </div>
-  );
-};
+  )
+}
 
+// This is how u should build the Components in the JSX file.

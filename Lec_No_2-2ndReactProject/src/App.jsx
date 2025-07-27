e4b597e0820ea32ce7337f064beb4916c681a854
Your Name <atharvakailaskadam1809@gmail.com>
2025-07-27 12:30:18 +0530

    Done an Dusted for Import and Export of the Components

--- Lec_No_2-2ndReactProject/src/App.jsx
+++ Lec_No_2-2ndReactProject/src/App.jsx
@@ -1,15 +1,20 @@
 // Now Using the React Fragments.
 
+// This import Name as 'NetflixSeries' can be anything.
+import { NetflixSeries, Footer, Header } from "./components/NetflixSeries";
+
 export let App = () => {
   return (
     // Can also use the '<Fragment>  </Fragment>' as a Parent div.
     <>
+      <Header />
       <NetflixSeries />
       <NetflixSeries />
       <NetflixSeries />
       <NetflixSeries />
       <NetflixSeries />
       <NetflixSeries />
+      <Footer />
     </>
   );
 };
@@ -18,27 +23,3 @@ export let App = () => {
 // 1. Variable Values.
 // 2. Expressions.
 // 3. Functions.
-function GenreFun ()
-{
-  return "Action , Drama , Action , Adventure";
-}
-
-const NetflixSeries = () => {
-  const Name = "Game of Thrones";
-  const Seasons = 8;
-  const Ratings = 9.2;
-  return (
-    <div>
-      <div>
-        <img src = "GOT.jpg" alt = "GOT" width = "40%" height = "40%" />
-      </div>
-      <h1> Name: {Name} </h1>
-      <h3> Seasons : {Seasons} </h3>
-      <h3> Ratings : {Ratings}/10 </h3>
-      <h3> Genre : {GenreFun()} </h3>
-      <p> <b>Game of Thrones :</b> is an epic fantasy television series based on George R.R. Martin’s book series A Song of Ice and Fire. Set in the fictional continents of Westeros and Essos, the story follows the power struggles among noble families as they vie for control of the Iron Throne. Blending political intrigue, war, betrayal, and mythical elements like dragons and White Walkers, the series explores themes of loyalty, ambition, and survival. Known for its complex characters, unexpected plot twists, and morally grey choices, Game of Thrones captivated a global audience and became one of the most iconic TV shows of the 21st century.</p>
-    </div>
-  )
-}
-
-// This is how u should build the Components in the JSX file.
--- Lec_No_3-3rdReactProject/src/App.jsx
+++ Lec_No_3-3rdReactProject/src/App.jsx
@@ -11,8 +11,17 @@ return (
 )
 };
 
+
+
+
+let age = prompt("Enter Your Age: ");
 const NetflixSeries = () =>
 {
+  let wishlist = 'Add to Wishlist';
+  if(age < 18)
+  {
+    wishlist = "Not Allowed";
+  }
   const Name = "Game of Thrones.";
   const Seasons = "8";
   const Ratings = "9.2";
@@ -21,7 +30,9 @@ const NetflixSeries = () =>
       <h1> Name : {Name} </h1>
       <h2> Seasons : {Seasons} </h2>
       <h2> Rating :{Ratings} </h2>
-      <h2> Genre : {GenreFun} </h2>
+      <h2> Genre : {GenreFun()} </h2>
+      <button> {age >= 18 ? 'Watch Now' : 'Not Allowed to Watch'} </button>
+      <button> {wishlist} </button>
     </div>
   )
 }
@@ -30,3 +41,5 @@ function GenreFun ()
 {
   return "Action, Thriller, Adventure";
 }
+
+// Conditionals in JSX

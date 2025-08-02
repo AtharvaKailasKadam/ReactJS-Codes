399778111dae149667f55200bfee2599c5be0f52
Your Name <atharvakailaskadam1809@gmail.com>
2025-08-02 17:50:10 +0530

    Done with Inline CSS

--- Lec_No_2-2ndReactProject/src/components/Lists.jsx
+++ Lec_No_2-2ndReactProject/src/components/Lists.jsx
@@ -7,12 +7,14 @@ export const SeriesCard = (props) => {
                 <div>
                     <img src = {imgUrl} alt = "IMAGE" width = "40%" height = "40%" />
                 </div>
-                <h3> Seasons : {season} </h3>
-                <h3> Episodes : {episode} </h3>
-                <h3> Ratings : {rating}/10 </h3>
-                <p> Genre : {genre} </p>
-                <p> Description : {description}</p>
-                <a href = {watchUrl} target = "_blank"> <button>Watch Now</button> </a>
+                <div>
+                    <h3> Seasons : {season} </h3>
+                    <h3> Episodes : {episode} </h3>
+                    <h3> Ratings : {rating}/10 </h3>
+                    <p> Genre : {genre} </p>
+                    <p style = {{fontSize : "15px"}}> Description : {description}</p>
+                    <a href = {watchUrl} target = "_blank"> <button>Watch Now</button> </a>
+                </div>
             </div>
         </li>
     );

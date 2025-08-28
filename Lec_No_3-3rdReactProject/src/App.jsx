9c07549e92a2db631abad924ccd99dfd3d3ee491
Your Name <atharvakailaskadam1809@gmail.com>
2025-08-28 20:07:46 +0530

    Done with Creating Toggle Project

--- Lec_No_3-3rdReactProject/src/App.jsx
+++ Lec_No_3-3rdReactProject/src/App.jsx
@@ -1,45 +1,11 @@
-export let App = () =>
+import { LiftingState } from "./Components/LiftingTheStateUp"
+export const App = () =>
 {
-return (
-  <>
-    <NetflixSeries />
-    <NetflixSeries />
-    <NetflixSeries />
-    <NetflixSeries />
-    <NetflixSeries />
-  </>
-)
-};
-
-let age = prompt("Enter Your Age: ");
-const NetflixSeries = () =>
-{
-  let wishlist = 'Add to Wishlist';
-  if(age < 18)
-  {
-    wishlist = "Not Allowed";
-  }
-  const Name = "Game of Thrones.";
-  const Seasons = "8";
-  const Ratings = "9.2";
   return(
-    <div>
-      <h1> Name : {Name} </h1>
-      <h2> Seasons : {Seasons} </h2>
-      <h2> Rating :{Ratings} </h2>
-      <h2> Genre : {GenreFun()} </h2>
-      <button> {age >= 18 ? 'Watch Now' : 'Not Allowed to Watch'} </button>
-      <button> {wishlist} </button>
-    </div>
+    <>
+    <section className = "Container">
+      <LiftingState />
+    </section>
+    </>
   )
 }
-
-function GenreFun ()
-{
-  return "Action, Thriller, Adventure";
-}
-
-// Conditionals in JSX
-
-// Using the Loops in React i.e. '.map()' method.
-
--- Lec_No_3-3rdReactProject/src/hooks/DerivedState.jsx
+++ Lec_No_3-3rdReactProject/src/hooks/DerivedState.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 export const DerivedState = () =>
 {
-    const [userInfo, setUserInfo] = useState([
+    const [userInfo] = useState([
     { Name: "Atharva", Age: 23, City: "Pune" },
     { Name: "John", Age: 30, City: "New York" },
     { Name: "Alice", Age: 28, City: "London" },
@@ -10,6 +10,7 @@ export const DerivedState = () =>
     { Name: "Charlie", Age: 22, City: "Los Angeles" },
     { Name: "David", Age: 25, City: "Chicago" },
     ])
+
     return (
         <div>
             <h1> User Information </h1>
